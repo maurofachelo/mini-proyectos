9082a202f3dfb2f25186cd43cbb22535be1708da
maurofachelo <maurofachelo@hotmail.com>
2025-08-10 15:05:44 -0300

    coversor de tiempo, se usa grid en css, DOM y eventos

---
+++ Conversor de Tiempo/main.js
@@ -0,0 +1,26 @@
+
+const contenedor = document.querySelector("input");
+const boton = document.querySelector("button");
+let liHoras = document.querySelector("#horas");
+let liMinutos = document.querySelector("#minutos");
+let liSegundos = document.querySelector("#segundos");
+let respuesta = document.querySelector("p")
+
+boton.addEventListener("click", hacerCalculo);
+
+function hacerCalculo(){
+    const dias = parseFloat(contenedor.value.trim())
+    const horas = dias *24;
+    const minutos = horas * 60;
+    const segundos = minutos * 60;
+    
+    respuesta.innerHTML = `"Se informa que ${dias} dia/s equivale a:"`
+
+    liHoras.textContent = `Horas: ${horas}`;
+    liMinutos.textContent = `Minutos: ${minutos}`;
+    liSegundos.textContent = `Segundos: ${segundos}`;
+}
+
+
+
+
